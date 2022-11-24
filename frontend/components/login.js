cd75ce7f7f4b09debf839c0c8428fa0c5fd41eb2
tomfantova <tom.fantova@gmail.com>
2022-11-24 12:38:15 +0100

    tom

--- frontend/components/login.js
+++ frontend/components/login.js
@@ -1,37 +1,15 @@
-import styles from '../styles/login.css';
+import styles from "../styles/Login.module.css";
 
-
-
-function login () {
-
-    
-
-
-
-
-
-
-
-
-
-
-    return (
-
-
-<div>
-<head>
-
-<title>See What's Happening</title>
-<h2>Join hacktweet today</h2>
-<button>Sign in</button>
-<h4>Already have an account ?</h4>
-<button>Sign up</button>
-
-</head>
-
-</div>
-
-
-
-    )
+function Login() {
+  return (
+    <div>
+      <head>
+        <title>See What's Happening</title>
+        <h2>Join hacktweet today</h2>
+        <button>Sign in</button>
+        <h4>Already have an account ?</h4>
+        <button>Sign up</button>
+      </head>
+    </div>
+  );
 }
