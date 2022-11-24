3ed9bb5586b66eda08096474bd879fe9c7f20763
tomfantova <tom.fantova@gmail.com>
2022-11-24 12:32:43 +0100

    tom

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,12 +1,23 @@
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Moment from "react-moment";
+import { Modal } from "antd";
+import Link from "next/link";
 
 function Home() {
   return (
     <div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.left}>
+          <img src="bird.png" className={styles.logo} />
+        </div>
+        <div className={styles.tweets}>
+          <h2>Home</h2>
+        </div>
+        <div className={styles.trends}>
+          <h2>Trends</h2>
+        </div>
       </main>
     </div>
   );
