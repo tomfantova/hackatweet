169726d7123d637281df1f08a0ec05616dcb73da
tomfantova <tom.fantova@gmail.com>
2022-11-24 09:39:23 +0100

    start

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,9 @@
-import '../styles/globals.css';
-import Head from 'next/head';
+import "../styles/globals.css";
+import Head from "next/head";
+import { Provider } from "react-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 function App({ Component, pageProps }) {
   return (
