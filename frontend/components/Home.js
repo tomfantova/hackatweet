b4e4c5f24c7d664937e05fac320241e9eba70892
tomfantova <tom.fantova@gmail.com>
2022-11-24 14:15:48 +0100

    tom

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,64 +1,26 @@
 import styles from "../styles/Home.module.css";
-import Tweet from "../components/Tweet";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Moment from "react-moment";
 import { Modal } from "antd";
 import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-  import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 function Home() {
- 
-  function Home() {
-    const handleClickSignin = () => {
-      console.log("coucou signin");
-    };
-  
-    const handleClickSignup = () => {
-      console.log("coucou signup");
-    };
-    return (
-      <div className={styles.container}>
+  return (
+    <div>
+      <main className={styles.main}>
         <div className={styles.left}>
-          <FontAwesomeIcon icon={faTwitter} className={styles.twitterIcon} />
+          <img src="bird.png" className={styles.logo} />
         </div>
-<<<<<<< HEAD
         <div className={styles.tweets}>
           <h2>Home</h2>
-          <Tweet />
-=======
-        <div className={styles.right}>
-          <div className={styles.formSection}>
-            <FontAwesomeIcon
-              icon={faTwitter}
-              className={styles.twitterIconSmall}
-            />
-            <div>
-              <h1 className={styles.title}>SEE WHAT'S HAPPENNING</h1>
-              <h4 className={styles.titleSmall}>Join Hackatweet today</h4>
-              <div
-                onClick={() => handleClickSignup()}
-                className={styles.btnSignup}
-              >
-                Sign Up
-              </div>
-              <h3 className={styles.titleAlready}>Already have an account ? </h3>
-              <div
-                onClick={() => handleClickSignin()}
-                className={styles.btnSignin}
-              >
-                Sign In
-              </div>
-            </div>
-          </div>
->>>>>>> ec138bb58b072cc43a1979d6a1bf3838e0035880
         </div>
-      </div>
-    );
-  }
-  
- 
+        <div className={styles.trends}>
+          <h2>Trends</h2>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Home;
---
+++ frontend/components/Login.js
@@ -0,0 +1,52 @@
+import styles from "../styles/Login.module.css";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Moment from "react-moment";
+import { Modal } from "antd";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+
+function Login() {
+  const handleClickSignin = () => {
+    console.log("coucou signin");
+  };
+
+  const handleClickSignup = () => {
+    console.log("coucou signup");
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <FontAwesomeIcon icon={faTwitter} className={styles.twitterIcon} />
+      </div>
+      <div className={styles.right}>
+        <div className={styles.formSection}>
+          <FontAwesomeIcon
+            icon={faTwitter}
+            className={styles.twitterIconSmall}
+          />
+          <div>
+            <h1 className={styles.title}>SEE WHAT'S HAPPENNING</h1>
+            <h4 className={styles.titleSmall}>Join Hackatweet today</h4>
+            <div
+              onClick={() => handleClickSignup()}
+              className={styles.btnSignup}
+            >
+              Sign Up
+            </div>
+            <h3 className={styles.titleAlready}>Already have an account ? </h3>
+            <div
+              onClick={() => handleClickSignin()}
+              className={styles.btnSignin}
+            >
+              Sign In
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
