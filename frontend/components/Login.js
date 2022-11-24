a04c78ebe2c771fe369f2a4f0aa1dbca1d6f4a22
chenanef <ychenanef@gmail.com>
2022-11-24 16:32:51 +0100

    signin

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -1,69 +1,77 @@
-
-import React, { useState } from 'react'
-import styles from '../styles/Login.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import { useDispatch } from 'react-redux';
-
+import React, { useState } from "react";
+import styles from "../styles/Login.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch } from "react-redux";
 
 export default function Login() {
-
   const dispatch = useDispatch();
 
-  const [signUpModal, setSignUpModal] = useState(false)
-  const [signInModal, setSignInModal] = useState(false)
-  
-
-
-
+  const [signUpModal, setSignUpModal] = useState(false);
+  const [signInModal, setSignInModal] = useState(false);
 
   const handleSignUp = () => {
-    signInModal && setSignInModal(false)
-    setSignUpModal(true)
-  }
+    signUpModal && setSignUpModal(false);
+    setSignUpModal(true);
+  };
 
   const handleSignIn = () => {
-    signUpModal && setSignUpModal(false)
-    setSignInModal(true)
-  }
+    signInModal && setSignInModal(false);
+    setSignInModal(true);
+  };
 
   const signUpModalContent = (
     <div className={styles.modalContainer}>
-        <FontAwesomeIcon icon={faXmark} className={styles.closeIcon} onClick={() => setSignUpModal(false)}/>
-        <FontAwesomeIcon icon={faTwitter} className={styles.twitterIcon} />
-        <h2>Create your Hackatweet account</h2>
-        <input type="text" placeholder='Firstname'/>
-        <input type="text" placeholder='Username'/>
-        <input type="password" placeholder='Password'/>
-        <div className={styles.signUpModalBtn}>Sign up</div>
+      <FontAwesomeIcon
+        icon={faXmark}
+        className={styles.closeIcon}
+        onClick={() => setSignUpModal(false)}
+      />
+      <FontAwesomeIcon icon={faTwitter} className={styles.twitterIcon} />
+      <h2>Create your Hackatweet account</h2>
+      <input type="text" placeholder="Firstname" />
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <div className={styles.signUpModalBtn}>Sign up</div>
     </div>
-  )
+  );
 
   const signInModalContent = (
     <div className={styles.modalContainer}>
-        <FontAwesomeIcon icon={faXmark} className={styles.closeIcon} onClick={() => setSignInModal(false)}/>
-        <FontAwesomeIcon icon={faTwitter} className={styles.twitterIcon} />
-        <h2>Log In to your Hackatweet account</h2>
-        <input type="text" placeholder='Username'/>
-        <input type="password" placeholder='Password'/>
-        <div className={styles.signUpModalBtn}>Sign In</div>
+      <FontAwesomeIcon
+        icon={faXmark}
+        className={styles.closeIcon}
+        onClick={() => setSignInModal(false)}
+      />
+      <FontAwesomeIcon icon={faTwitter} className={styles.twitterIcon} />
+      <h2>Log In to your Hackatweet account</h2>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <div className={styles.signUpModalBtn}>Sign In</div>
     </div>
-  )
+  );
 
   return (
     <div className={styles.globalContainer}>
-        {signUpModal && signUpModalContent}
-        <div className={styles.imageContainer}></div>
-        <div className={styles.textContainer}>
-            <FontAwesomeIcon icon={faTwitter} className={styles.twitterIcon} />
-            <h2>See what's <br/>happening</h2>
-            <h4>Join Hackatweet today</h4>
-            <div className={styles.signUpBtn} onClick={() => handleSignUp()}>Sign up</div>
-            <p>Already have an account?</p>
-            <div className={styles.signUpBtn} onClick={() => handleSignIn()}>Sign in</div>
-                
+      {signUpModal && signUpModalContent}
+      {signInModal && signInModalContent}
+      <div className={styles.imageContainer}></div>
+      <div className={styles.textContainer}>
+        <FontAwesomeIcon icon={faTwitter} className={styles.twitterIcon} />
+        <h2>
+          See what's <br />
+          happening
+        </h2>
+        <h4>Join Hackatweet today</h4>
+        <div className={styles.signUpBtn} onClick={() => handleSignUp()}>
+          Sign up
         </div>
+        <p>Already have an account?</p>
+        <div className={styles.signUpBtn} onClick={() => handleSignIn()}>
+          Sign in
+        </div>
+      </div>
     </div>
-  )
+  );
 }
